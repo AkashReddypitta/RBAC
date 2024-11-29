28144d1da503619a22fef7ab2d2861a92561d6d1
Akash <akashreddypitta@gmail.com>
2024-11-29 19:39:35 +0530

    Add files via upload

---
+++ static/js/employee.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+    $("#write").on("click", function() {  
+      
+      window.open("https://codepen.io/kimonodev/full/yKVpzQ/");  
+    
+      }); 
+      
+     
+      
+    
+    });
